d84aaae0c6c75567453ce44c51d6a683b689219f
Nour Souid <noursouidd@gmail.com>
2020-02-24 16:10:26 -0500

    validation for the service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,9 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
+  validates :title, presence: true
+  validates :description, length: { in: 20..40 }
+  validates :rate, presence: true, numericality: true
+  validates :years_experience, numericality: true
+
 end
