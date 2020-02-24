4b15f87aa0adcb5ac9bea3d7a38358e1fb00a08e
Nour Souid <noursouidd@gmail.com>
2020-02-24 17:42:33 -0500

    Booking controller methods new create edit update added views for edit and new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,32 @@
 class BookingsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @service = Service.find(:service_id)
+    @booking = Booking.new(secure_params)
+    @booking.service = @service
+    if @booking.save
+      # maybe change it to redirect to the dashbord
+      rediret_to service_path(@service)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = Booking.find(prams[:id])
+  end
+
+  def update
+    @booking = Booking.find(prams[:id])
+    @booking.update(params[:booking])
+  end
+
+  def secure_params
+    # check the user_id
+    parms_require(:booking).permit(:start, :end, :message)
+  end
 end
