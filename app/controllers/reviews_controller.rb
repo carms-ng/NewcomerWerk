bf34b633f7b659e4de1650b56b0ad34cd743d206
Laurence Berube Cote <lberubecote@hotmail.com>
2020-02-25 11:36:52 -0500

    review in progress

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+  def new
+  end
+
+  def create
+    @booking = Booking.new(params[:booking_id])
+    @review = Review.new(review_params)
+    review.booking = booking
+    if review.save
+      redirect_to booking_path(@booking)
+    else
+      render: new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
