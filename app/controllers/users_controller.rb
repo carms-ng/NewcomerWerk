07d3ce3e4b660e6b192b7e48a0eb54ee9bf82421
Carms Ng <carmenhyng89@gmail.com>
2020-02-25 13:21:05 -0500

    user profile page reviewed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,14 @@
 class UsersController < ApplicationController
+
   def index
   end
 
   def show
-    @user  = User.find(current_user[:id])
+    @user = current_user
   end
 
-  def update
-    @user = User.find(current_user[:id])
-    @user.update(user_params)
-    redirect_to profile_path(@user)
+  def edit
+
   end
 
-  private
-
-  def user_params
-    params.required(:user).permit(:first_name, :last_name, :photo, :description, :location)
-  end
 end
