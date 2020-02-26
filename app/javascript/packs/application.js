e4ed834cf77a88b7bd4ecd095f1c4c9de8ace2ac
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-26 16:15:59 -0500

    working on tabs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import "bootstrap";
 
 // window.submit = submit;
 // onclick="window.submit()"
+<<<<<<< HEAD
 
 
 
+=======
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
+>>>>>>> ab5e841bc1af64a963f361244527f904fc914f07
