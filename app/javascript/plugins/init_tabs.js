95615b8a9c5b920536f06d5c2dbf4f8c8fba225e
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-27 15:18:24 -0500

    Fixed footer, profile and buttons

---
+++ app/javascript/plugins/init_tabs.js
@@ -0,0 +1,9 @@
+const received = document.getElementById('tab-review-received');
+received.addEventListener("click", (event) => {
+  console.log(event);
+  console.log(event.currentTarget);
+});
+
+
+const written = document.getElementById('tab-review-written')
+
