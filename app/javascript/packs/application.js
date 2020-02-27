a50807a80216a5fec1c1f168bf4cc216a3790f05
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-27 17:40:45 -0500

    Updated files with tabs javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,11 @@ import "../plugins/flatpickr"
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
-import { loadModal } from '../components/modal'
+import { loadModal } from '../components/modal';
+import { receivedReview, writtenReview } from '../plugins/init_tabs';
 
 initMapbox();
 loadModal();
 
+window.receivedReview = receivedReview
+window.writtenReview = writtenReview
--- app/javascript/plugins/init_tabs.js
+++ app/javascript/plugins/init_tabs.js
@@ -1,9 +1,28 @@
-const received = document.getElementById('tab-review-received');
-received.addEventListener("click", (event) => {
-  console.log(event);
-  console.log(event.currentTarget);
-});
 
+// received.addEventListener("click", (event) => {
+//   console.log(event);
+// });
 
-const written = document.getElementById('tab-review-written')
+const received = document.getElementById('received-review');
+const written = document.getElementById('reviews-written');
+
+export const receivedReview = () => {
+  if (received.style.display === "none") {
+    received.style.display = "block"
+    written.style.display = "none"
+  } else {
+    received.style.display = "none"
+    written.style.display = "block"
+  };
+};
+
+export const writtenReview = () => {
+  if (written.style.display === "none") {
+    written.style.display = "block"
+    received.style.display = "none"
+  } else {
+    written.style.display = "none"
+    received.style.display = "block"
+  };
+};
 
