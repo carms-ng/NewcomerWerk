835e0127abde212a68be7f7f6f08773fb71be6a8
Nour Souid <noursouidd@gmail.com>
2020-02-28 09:16:31 -0500

    commented the javascript so I can work on it on another branch

--- app/javascript/components/buttons_effects.js
+++ app/javascript/components/buttons_effects.js
@@ -1,12 +1,12 @@
-const effect = () => {
-  const accept = document.getElementById('accept');
+// const effect = () => {
+//   const accept = document.getElementById('accept');
 
-    accept.addEventListener("click", (event) => {
-      event.preventDefault();
-      console.log(accept);
-      document.querySelector('.requests-card').style.box_shadow = '0 0 15px #230444';
-    });
+//     accept.addEventListener("click", (event) => {
+//       event.preventDefault();
+//       console.log(accept);
+//       document.querySelector('.requests-card').style.box_shadow = '0 0 15px #230444';
+//     });
 
-}
+// }
 
-export { effect }
+// export { effect }
