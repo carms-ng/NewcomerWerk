2fc2757b02c0bd452223c3713e17916d65728381
Iphona-Clemence Paiement <iphona.paiement@hotmail.com>
2020-02-28 13:24:11 -0500

    Added Javascript and fixed tabs styling profile reviews.

--- app/javascript/plugins/init_tabs.js
+++ app/javascript/plugins/init_tabs.js
@@ -3,16 +3,38 @@
 //   console.log(event);
 // });
 
+// Reviews Tabs
+const tabReviewReceived = document.getElementById('tab-review-received');
+const tabReviewWritten = document.getElementById('tab-review-written');
+
+// Reviews view
 const received = document.getElementById('received-review');
 const written = document.getElementById('reviews-written');
 
+
+// Reviews View & tabs functions
 export const receivedReview = () => {
+  // To display received review
   if (received.style.display === "none") {
     received.style.display = "block"
     written.style.display = "none"
-  } else {
+    // highlight this tab
+    tabReviewReceived.style.backgroundColor = "#230444"
+    tabReviewReceived.style.color = "#FFFFFF"
+    // unhighlight the other tab
+    tabReviewWritten.style.backgroundColor = "#FFFFFF"
+    tabReviewWritten.style.color = "#230444"
+  }
+  // To hide received reviews and show written reviews
+  else {
     received.style.display = "none"
     written.style.display = "block"
+    // unhighlight this tab
+    tabReviewReceived.style.backgroundColor = "#FFFFFF"
+    tabReviewReceived.style.color = "#230444"
+    // highlight the other tab
+    tabReviewWritten.style.backgroundColor = "#FFFFFF"
+    tabReviewWritten.style.color = "#230444"
   };
 };
 
@@ -20,9 +42,21 @@ export const writtenReview = () => {
   if (written.style.display === "none") {
     written.style.display = "block"
     received.style.display = "none"
+    // highlight this tab
+    tabReviewWritten.style.backgroundColor = "#230444"
+    tabReviewWritten.style.color = "#FFFFFF"
+    // unhighlight the other tab
+    tabReviewReceived.style.backgroundColor = "#FFFFFF"
+    tabReviewReceived.style.color = "#230444"
   } else {
     written.style.display = "none"
     received.style.display = "block"
+    // unhighlight this tab
+    tabReviewWritten.style.backgroundColor = "#FFFFFF"
+    tabReviewWritten.style.color = "#230444"
+    // highlight the other tab
+    tabReviewReceived.style.backgroundColor = "#230444"
+    tabReviewReceived.style.color = "#FFFFFF"
   };
 };
 
